84832693b3cd6f334ee410a208864a6e4260b138
Lewis <lewisyoul@gmail.com>
2018-02-14 16:31:14 +0000

    move HTML from template to templateUrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Hey guys!</h1>
-  <ul>
-    <li *ngFor="let things of thingsIlike">{{ things }}</li>
-  </ul>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
