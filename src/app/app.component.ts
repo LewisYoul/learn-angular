6ddd979a61442b4b99fb45afc71f1f80bf5da956
Lewis <lewisyoul@gmail.com>
2018-02-14 16:29:42 +0000

    use interpolation to iterate over template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>Hey guys!</h1>
+  <ul>
+    <li *ngFor="let things of thingsIlike">{{ things }}</li>
+  </ul>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'My first Angular App!';
+  thingsIlike = ['Cycling', 'Tennis', 'Programming'];
 }
