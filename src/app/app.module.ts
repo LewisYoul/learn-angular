3fbd7b7e1b8597e889cca2ea357afd1327c92af7
Lewis <lewisyoul@gmail.com>
2018-02-14 16:00:11 +0000

    create sub-heading component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SubHeadingComponent } from './sub-heading/sub-heading.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubHeadingComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sub-heading/sub-heading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-heading',
+  templateUrl: './sub-heading.component.html',
+  styleUrls: ['./sub-heading.component.css']
+})
+export class SubHeadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
